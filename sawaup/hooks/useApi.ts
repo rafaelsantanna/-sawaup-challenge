1174530585fffc3071d0e04a271f0c4d3e6329e1
Rafael Sant Anna <rafaeel.16@gmail.com>
2022-10-02 21:07:40 +0100

    Hooks useAPI

---
+++ sawaup/hooks/useApi.ts
@@ -0,0 +1,28 @@
+import { useState, useEffect, useCallback } from 'react'
+import type { HttpRequest } from '../types/http-request'
+import { httpGet } from '../lib/fetchData'
+
+function useApi<T>(request: HttpRequest): [boolean, T] {
+  const [loading, setLoading] = useState<boolean>(true)
+  const [data, setData] = useState<T | any>(null)
+
+  const fetchApi = () => {
+    fetch(`http://127.0.0.1:8080/api${request.route}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((json) => {
+        console.log(json);
+        setLoading(false);
+        setData(json);
+      });
+  }
+
+  useEffect(() => {
+    fetchApi()
+  }, [])
+
+  return [loading, data]
+}
+
+export default useApi
